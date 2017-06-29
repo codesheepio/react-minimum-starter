fbd5181365e7241c64e1a9c120dc28b2b6eb7b4b
Arnupharp Viratanapanu <arnupharp@larngeartech.com>
2017-06-29 23:51:17 +0700

    Hot reload setting

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 var path = require('path')
+var webpack = require('webpack')
+
 module.exports = {
-  entry: path.resolve('src/index.js'),
+  devtool: 'eval-source-map',
+  entry: ['react-hot-loader/patch', path.resolve('src/index.js')],
   output: {
     filename: 'bundle.js',
     path: path.resolve('dist'),
@@ -11,11 +14,27 @@ module.exports = {
       {
         test: /\.js$/,
         use: ['babel-loader'],
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/,
       },
     ],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    // enable HMR globally
+
+    new webpack.NamedModulesPlugin(),
+    // prints more readable module names in the browser console on HMR updates
+
+    new webpack.NoEmitOnErrorsPlugin(),
+    // do not emit compiled assets that include errors
+  ],
+  devServer: {
+    hot: true,
+    historyApiFallback: true,
   },
 }
