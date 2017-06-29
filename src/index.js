5d2fdfec30ec48b5b767ab83493e38f6dd062051
Arnupharp Viratanapanu <arnupharp@larngeartech.com>
2017-06-29 23:36:50 +0700

    Setup webpack

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles.css'
+
+ReactDOM.render(
+  <div className="title">Hello, react</div>,
+  document.getElementById('react-root')
+)
